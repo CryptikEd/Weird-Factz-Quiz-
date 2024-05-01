194984769052f693ba4980909620f4ae782d3496
Edwin Kigen <kubwayokigen@gmail.com>
2024-05-01 14:43:27 +0000

    Adding event listner for lets begin button and hiding the welcome page and calling beforewestart class.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    
+    document.getElementById("beginBtn").addEventListener("click", function() {
+        
+        document.getElementById("welcome").style.display = "none";
+      
+        document.getElementById("beforewestart").style.display = "block";
+    }); 
     
 });
