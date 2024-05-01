603f823809edc2e396fd00d42188d50b317e36d5
Edwin Kigen <kubwayokigen@gmail.com>
2024-05-01 15:32:43 +0000

    Add back button functionality. Implement going back to welcome page.Added ability to go back to welcome page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById("beforewestart").style.display = "block";
     }); 
     
-
+        // Event listener for the "readyBtn" button
     document.getElementById("readyBtn").addEventListener("click", function() {
         var playerName = document.getElementById("nameInput").value;
         if (playerName.trim() === "") {
@@ -24,6 +24,13 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById("playerNameDisplay").textContent = playerName;
         }
     });
-
-
+        // Event listener for the "Back" button
+    document.getElementById("backBtn").addEventListener("click", function() {
+        // Show the welcome page
+        document.getElementById("welcome").style.display = "block";
+      
+        // Hide the quiz introduction page
+        document.getElementById("beforewestart").style.display = "none";
+      });
+    
 });
